065c0033928c12c01f4c5889ead06f59c150149b
Coita Daria <dariacoita95@gmail.com>
2025-11-23 13:48:18 +0200

    Add files via upload

---
+++ Problema_8/Problema_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Problema_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pentru doua numere a si b introduse de la tastatura, se afiseaza inversarea valorilor lor fara a folosi alte variabile suplimentare.");
+            Console.Write("Introduceti numarul a: ");
+            int a = Citire("a");
+            Console.Write("Introduceti numarul b: ");
+            int b = Citire("b");
+            (a, b) = (b, a);
+            Console.WriteLine($"Dupa inversare, a = {a} si b = {b}.");
+        }
+        static int Citire(string a)
+        {
+            while (true)
+            {
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Eroare: Introduceti un numar valid.");
+                    return Citire(a);
+                }
+            }
+        }
+    }
+}
