e68cb06ec1b3983e522270786c4404be3f4d8703
Coita Daria <dariacoita95@gmail.com>
2025-11-23 13:53:49 +0200

    Add files via upload

---
+++ Problema_11/Problema_11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Problema_11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Programul afiseaza in ordine inversa cifrele unui numar intreg n citit de la tastatura.");
+            int x = Citire("x");
+            Console.WriteLine($"Cifrele numarului {x} in ordine inversa sunt:");
+            if (x == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (x > 0)
+                {
+                    while (x > 0)
+                    {
+                        Console.Write(x % 10 + " ");
+                        x = x / 10;
+                    }
+                }
+                else
+                {
+                    x = -x;
+                    while (x > 0)
+                    {
+                        Console.Write(x % 10 + " ");
+                        x = x / 10;
+                    }
+                    Console.Write("(-)");
+                }
+            }
+        }
+        static int Citire(string x)
+        {
+            Console.WriteLine("Introduceti un numar intreg:");
+            while (true)
+            {
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Numar invalid. Introduceti un numar intreg valid:");
+                    return Citire(x);
+                }
+            }
+        }
+    }
+}
