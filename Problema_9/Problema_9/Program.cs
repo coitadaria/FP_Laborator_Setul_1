85b4642d7081d19245b65331528689086ef10a61
Coita Daria <dariacoita95@gmail.com>
2025-11-23 13:49:18 +0200

    Add files via upload

---
+++ Problema_9/Problema_9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Problema_9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Programul afiseaza toti divizori numarului pozitiv, intreg n introdus de la tastatura.");
+            int n = Citire("n");
+            if (n == 0)
+                Console.WriteLine("Divizorii numarului 0 sunt toate numerele intregi nenule.");
+            if (n == 1) Console.WriteLine("Divizorul numarului 1 este 1.");
+            List<int> divizori = new List<int>();
+            divizori.Add(1);
+            divizori.Add(n);
+            int d;
+            for (d = 2; d * d < n; d++)
+                if (n % d == 0)
+                {
+                    divizori.Add(d);
+                    divizori.Add(n / d);
+                }
+            if (d * d == n)
+                divizori.Add(d);
+            divizori.Sort();
+            Console.WriteLine($"Divizorii pozitivi a numarului {n} sunt:");
+            foreach (int divizor in divizori)
+                Console.Write(divizor + " ");
+        }
+        static int Citire(string n)
+        {
+            while (true)
+            {
+                Console.WriteLine("Introduceti un numar intreg pozitiv:");
+                try
+                {
+                    string numar = Console.ReadLine();
+                    int x = int.Parse(numar);
+                    if (x > 0)
+                    {
+                        return x;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numarul introdus nu este pozitiv.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input invalid. Va rugam sa introduceti un numar intreg pozitiv.");
+                }
+            }
+        }
+    }
+}
